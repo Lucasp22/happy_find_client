384302d38a9fea217b501d2f3dc402b48c6a7f71
Lucas Padua <lucas.p2222@gmail.ciom>
2018-08-08 10:29:33 +1000

    Update pages

---
+++ src/components/OrderSummary.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class OrderSummary extends Component {
+  render() {
+    return(
+      <main>
+        <h1>Order Summary</h1>
+      </main>
+    )
+  }
+}
+
+export default OrderSummary;
