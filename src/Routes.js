e46a52207d9cadd8e1a30ce18f3f68b2b57b6c83
Tim Caldwell <tim.caldwell@einti.com>
2018-08-08 12:15:54 +1000

    fix routes post merge

--- src/Routes.js
+++ src/Routes.js
@@ -17,15 +17,12 @@ const Routes = (
     <div>
       <Route exact path="/*" component={NavBar} /> {/* header */}
       <Switch> {/* only one of these will display at a time */}
-      <Route path = "/booking" component={Booking} />
-      <Route path = "/checkoutform" component={CheckoutForm} />
-      <Route path = "/login" component={ Login } />
-      <Route path = "/signup" component={ SignUp } />
-      <Route path = "/paysummary" component={PaySummary} />
-      <Route path = "/" component={Home} />
+        <Route path = "/booking" component={Booking} />
+        <Route path = "/login" component={ Login } />
+        <Route path = "/signup" component={ SignUp } />
+        <Route path = "/" component={Home} />
       </Switch>
       <Route exact path="/*" component={Footer} /> {/* footer */}
-
     </div>
   </Router>
 )
