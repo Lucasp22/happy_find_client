2538b4f1f0b124ef2603326ea5d8bb3143ec8e97
Lucas Padua <lucas.p2222@gmail.ciom>
2018-08-08 14:24:28 +1000

    Displau Summary

--- src/components/CustomerSummary.js
+++ src/components/CustomerSummary.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 
+
+
+
 class CustomerSummary extends Component {
 
   render() {
-
+    const { user_name, user_email, user_phone, user_address, hours, date } = this.props.customer || { user_name: 'testing', user_email: 'test@test.co',user_phone: '111', user_address: 'xxx', hours: '1', date: '1984-22-2' }
     return(
       <div>
         <h1>Customer Details SUmmary Here</h1>
+        { user_name }/{ user_email }/{ user_phone }/{ user_address }/{ hours }/{ date }
       </div>
     )
   }
