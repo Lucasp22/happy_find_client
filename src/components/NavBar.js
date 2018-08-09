6f12f84db4ecc0a5551e5ae8c1717d9de1db093f
Tim Caldwell <tim.caldwell@einti.com>
2018-08-09 15:54:14 +1000

    redo css/html for header to simplify

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -47,47 +47,36 @@ class NavBar extends Component {
   render() {
     return(
       <header>
-        <div class="navWraper" id="home">
-         <div class="clearfix">
-            <h2 class="companyName">Happy Find</h2>
-            <nav class="mainNav clearfix">
-                  {
-                   window.localStorage.jwtToken ? (
-                  <div>
-                    <ul>
-                      <li>
-                        <Link to = {"/"}>Edit Profile</Link>
-                      </li>
-                      <li>
-                        <Link to ="/">Home</Link>
-                      </li>
-                      <li>
-                        <Link to ="/" onClick={this._signOut}>Sign out</Link>
-                      </li>
-                    </ul>
-                  </div>
-                     ) : (
-                  <div>
-                    <ul>
-                      <li>
-                        <Link to={`/home`}>Home</Link>
-                      </li>
-                      <li>
-                        <Link to={`/signup`}>Sign Up</Link>
-                      </li>
-                      <li>
-                        <Link to={`/login`}>Log in</Link>
-                      </li>
-
-                    </ul>
-
-
-                  </div>
-               )
-              }
-            </nav>
-          </div>
-        </div>
+        <h2 class="companyName">Happy Find</h2>
+        <nav class="mainNav">
+          {
+            window.localStorage.jwtToken ? (
+            <ul>
+              <li>
+                <Link to = {"/edit"}>Edit Profile</Link>
+              </li>
+              <li>
+                <Link to ="/">Home</Link>
+              </li>
+              <li>
+                <Link to ="/" onClick={this._signOut}>Sign out</Link>
+              </li>
+            </ul>
+              ) : (
+            <ul>
+              <li>
+                <Link to={`/home`}>Home</Link>
+              </li>
+              <li>
+                <Link to={`/signup`}>Sign Up</Link>
+              </li>
+              <li>
+                <Link to={`/login`}>Log in</Link>
+              </li>
+            </ul>
+           )
+         }
+       </nav>
       </header>
     );
   }
